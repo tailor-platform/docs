515853dbe3211062af092e6ab16d74fa0480584c
Anukiran <anukiransolur@gmail.com>
2026-04-07 13:55:34 -0500

    Update releases.data.ts

--- docs/reference/changelog/releases.data.ts
+++ docs/reference/changelog/releases.data.ts
@@ -1,6 +1,9 @@
 import type { ChangelogData } from '../../../.vitepress/theme/composables/useChangelog'
 import releasesJson from './releases.json'
 
+declare const data: ChangelogData
+export { data }
+
 export default {
   load(): ChangelogData {
     return releasesJson as unknown as ChangelogData
