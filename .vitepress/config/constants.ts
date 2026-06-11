ff00ca592e1b4ffc0d7c14359cf8b5eee2234316
Anukiran <anukiransolur@gmail.com>
2026-06-11 14:19:29 -0500

    order guides sidebar items

--- .vitepress/config/constants.ts
+++ .vitepress/config/constants.ts
@@ -87,6 +87,18 @@ export const defaultSidebarOrder: string[] = ["overview", "quickstart"];
 
 // Section-specific overrides (only if you need different ordering for a specific section)
 export const sidebarItemOrder: Record<string, string[]> = {
+  guides: [
+    "application",
+    "events",
+    "resolver",
+    "secretmanager",
+    "static-website-hosting",
+    "opentelemetry",
+    "tailordb",
+    "executor",
+    "function",
+    "auth",
+  ],
   "setup-auth": [
     "overview",
     "setup-identity-provider",
